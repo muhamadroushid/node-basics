f2eaabd96ed99f3fa77bd19971584f9d656d1b71
muhamadroushid <muhamadroushid@gmail.com>
2025-05-15 21:50:27 +0530

    error fixed: findOne to find

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/api/genres', (req, res) => {
 // Update a genre
 
 app.put('/api/genres/:id', (req, res) => {
-  const genre = genres.findOne(g => g.id === parseInt(req.params.id));
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
   if (!genre)
     return res.status(404).send('The genre with the given ID was not found');
 
